80b6b8f19742a5e212a2ac746f2f7ea7dc9e0e7d
Kayden <kayden.adams@colorado.edu>
2020-02-25 12:05:22 -0700

    Modifies dungeon map to hold cols and rows

--- Maps/DungeonMap.py
+++ Maps/DungeonMap.py
@@ -3,17 +3,18 @@ import arcade
 FLOOR = arcade.color.ASH_GREY
 WALL = arcade.color.BLACK
 
-
-class DungeonMap:
+class DungeonMap():
     def __init__(self, rows, cols):
+        self.rows = rows
+        self.cols = cols
         self.grid = []
-        for row in range(0, rows):
+        for row in range(self.rows):
             self.grid.append([])
 
-            for col in range(0, cols):
+            for col in range(self.cols):
                 self.grid[row].append(WALL) #start off with every square a wall
 
-        #make the inside floor
-        for row in range(1, rows-1):
-            for col in range(1, cols-1):
+        #make the inside floor tiles
+        for row in range(1, self.rows-1):
+            for col in range(1, self.cols-1):
                 self.grid[row][col] = FLOOR
