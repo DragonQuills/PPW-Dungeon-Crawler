231b65a5a6559b9637435553956bf9c0c0d86560
Kayden <kayden.adams@colorado.edu>
2020-03-10 15:48:49 -0600

    Adds subclasses with different colors for their rectangles

--- Actors/Monster.py
+++ Actors/Monster.py
@@ -13,3 +13,15 @@ class Monster(arcade.Sprite):
     def move(self, direction):
         self.x += direction[0]
         self.y += direction[1]
+
+class SkullMonster(Monster):
+    def __init__(self, x, y):
+        super().__init__(x, y, arcade.color.BONE)
+
+class LampMonster(Monster):
+    def __init__(self, x, y):
+        super().__init__(x, y, arcade.color.ALLOY_ORANGE)
+
+class FishMonster(Monster):
+    def __init__(self, x, y):
+        super().__init__(x, y, arcade.color.AIR_SUPERIORITY_BLUE)
