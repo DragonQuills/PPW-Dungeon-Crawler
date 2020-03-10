55bfd832f4fc45ae912c1232c02f1d3857eebc89
Kayden <kayden.adams@colorado.edu>
2020-03-10 15:45:09 -0600

    Ads skeleton for monster class

--- Actors/Monster.py
+++ Actors/Monster.py
@@ -1,2 +1,15 @@
-class Monster:
-    pass
+from definitions import *
+
+class Monster(arcade.Sprite):
+    def __init__(self, x, y, color):
+        self.x = x
+        self.y = y
+        self.color = color
+
+    def draw(self):
+        icon = arcade.create_rectangle_filled(self.x, self.y, WIDTH, HEIGHT, self.color)
+        icon.draw()
+
+    def move(self, direction):
+        self.x += direction[0]
+        self.y += direction[1]
