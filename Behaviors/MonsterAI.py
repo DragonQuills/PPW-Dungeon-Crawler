c5324c80c8d8c9c3ffa32f133318485c72ab0102
Kayden <kayden.adams@colorado.edu>
2020-03-11 19:01:32 -0600

    Adds skeleton for a monsterAI

---
+++ Behaviors/MonsterAI.py
@@ -0,0 +1,14 @@
+from definitions import *
+
+class MonsterAI:
+    def __init__(self, dungeon_map):
+        self.dungeon_map = dungeon_map
+
+    def solve(monster, player):
+        pass
+
+    def reconstruct_path(monster, player, prev):
+        pass
+
+    def next_move(monster, player):
+        pass
