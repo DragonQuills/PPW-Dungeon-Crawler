3785a9093800f0a72eafb362fe42f24e4a333e84
Kayden <kayden.adams@colorado.edu>
2020-03-30 18:25:45 -0600

    Adds a little square to signify the direction an actor is looking

--- Actors/Actor.py
+++ Actors/Actor.py
@@ -5,6 +5,7 @@ class Actor(arcade.Sprite):
         self.row = row
         self.col = col
         self._color = color
+        self.facing = DOWN
 
     def get_x_y(self):
         x = (MARGIN + WIDTH) * self.col + MARGIN + WIDTH // 2
@@ -15,7 +16,12 @@ class Actor(arcade.Sprite):
         x, y = self.get_x_y()
         icon = arcade.create_rectangle_filled(x, y, WIDTH, HEIGHT, self.color)
         icon.draw()
+        face = arcade.create_rectangle_filled(x+self.facing[1]*27, y+self.facing[0]*27, 10, 10, arcade.color.AFRICAN_VIOLET)
+        face.draw()
 
     def move(self, direction):
         self.row += direction[0]
         self.col += direction[1]
+
+    def change_facing(self, direction):
+        self.facing = direction
