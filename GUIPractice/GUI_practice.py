74435b88cca57604e2b2ef4467c79a9dde0f12f3
Kayden <kayden.adams@colorado.edu>
2020-04-12 14:05:01 -0600

    Adds a textbox to practice with

--- GUIPractice/GUI_practice.py
+++ GUIPractice/GUI_practice.py
@@ -1,17 +1,16 @@
 import arcade
 
 
+
 class Window(arcade.Window):
     def __init__(self):
         super().__init__(800, 600)
-        self.text = ""
-        self.center_x = self.width / 2
-        self.center_y = self.height / 2
 
     def setup(self):
         arcade.set_background_color(arcade.color.AMETHYST)
-        self.text_box = arcade.Sprite(":resources:gui_themes/Fantasy/TextBox/LightBrown.png")
-
+        self.text_box = arcade.Sprite(":resources:gui_themes/Fantasy/TextBox/Brown.png", scale = 1.7, center_x = 400, center_y = 300)
+        # self.text_box.height = 96
+        # self.text_box.width = 800
     def on_draw(self):
         arcade.start_render()
         super().on_draw()
