b71b8ebe0ecb92fcdd4b07237772798e88a625f6
Kayden <kayden.adams@colorado.edu>
2020-04-14 17:02:53 -0600

    Adds test that MessageLogger is a Singleton

--- tests/message_logger_test.py
+++ tests/message_logger_test.py
@@ -1,2 +1,9 @@
-def test_add_message():
-    
+import pytest
+from UI.MessageLogger import MessageLogger
+
+def test_messagelogger_is_singleton():
+    with pytest.raises(RuntimeError):
+        m1 = MessageLogger()
+    m2 = MessageLogger.instance()
+    m3 = MessageLogger.instance()
+    assert(m2 is m3)
