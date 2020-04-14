30de5e5eb699464ea2c096580e68bd1445774c19
Kayden <kayden.adams@colorado.edu>
2020-04-14 17:46:22 -0600

    Adds test for push_message

--- tests/message_logger_test.py
+++ tests/message_logger_test.py
@@ -7,3 +7,12 @@ def test_messagelogger_is_singleton():
     m2 = MessageLogger.instance()
     m3 = MessageLogger.instance()
     assert(m2 is m3)
+
+def test_push_message():
+    MAX_MESSAGES_ON_SCREEN = 2
+    m = MessageLogger()
+    m.push_message("test1")
+    m.push_message("test2")
+    assert(m.messages = ["test1, test2"])
+    m.push_message("test3")
+    assert(m.messages = ["test2, test3"])
