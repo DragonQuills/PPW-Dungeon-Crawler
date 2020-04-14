77a56f365b06119cd3c01520916051c4229f6c88
Kayden <kayden.adams@colorado.edu>
2020-04-14 17:03:25 -0600

    Adds MessageLoggger class with Singleton functionality

--- UI/MessageLogger.py
+++ UI/MessageLogger.py
@@ -1,1 +1,28 @@
-class MessageLogger
+'''
+A Singleton message logger to track and display
+messages to the text box
+
+Code to make this a singleton is from here:
+https://python-patterns.guide/gang-of-four/singleton/
+'''
+
+class MessageLogger():
+    _instance = None
+
+    def __init__(self):
+        raise RuntimeError('Call instance() instead')
+
+    @classmethod
+    def instance(cls):
+        if cls._instance is None:
+            print('Creating new instance')
+            cls._instance = cls.__new__(cls)
+            # Put any initialization here.
+            cls.messages = []
+        return cls._instance
+
+    def add_message(self, message):
+        self.messages.append(message)
+
+    def print_message(self):
+        print(self.messages)
