9215d1153db169ad66129a71148df5d016d22681
Kayden <kayden.adams@colorado.edu>
2020-04-18 11:18:31 -0600

    Adds find_location_for_monster implementation

--- Spawners/MonsterSpawner.py
+++ Spawners/MonsterSpawner.py
@@ -1,5 +1,6 @@
 from Actors.Monster import *
-from random import randint
+from Behaviors.MonsterAI import Position, MonsterAI
+from random import randint, choice
 
 class MonsterSpawner:
     def get_monster(self, monster_name, dungeon_map):
@@ -17,4 +18,17 @@ class MonsterSpawner:
         monster.attack += randint(-1, 1)
         monster.defense += randint(-1, 1)
 
+        return monster
+
+
+    def find_location_for_monster(self, monster, dungeon_map, player):
+        valid_locations = []
+        for row in range(0, ROW_COUNT):
+            for col in range(0, COLUMN_COUNT):
+                #quicker check first
+                if(dungeon_map.grid[row][col] == FLOOR and len(monster.ai.solve(monster, player)) >= 10):
+                    valid_locations.append(Position(row, col))
+        position = choice(valid_locations)
+        monster.row = position.row
+        monster.col = position.col
         return monster
