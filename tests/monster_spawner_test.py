51bbdc0eee78f0041f408440ddf09c3dc7ff997a
Kayden <kayden.adams@colorado.edu>
2020-04-18 10:59:11 -0600

    Adds test for find_location_for_monster

--- tests/monster_spawner_test.py
+++ tests/monster_spawner_test.py
@@ -1,3 +1,5 @@
+from Actors.Monster import FishMonster
+from Actors.Player import Player
 from Spawners.MonsterSpawner import MonsterSpawner
 from Maps.DungeonMap import DungeonMap
 
@@ -7,5 +9,23 @@ def test_spawner_get_monster():
 
     fish = spawner.get_monster("fish", dungeon)
 
+    assert( isinstance(fish, FishMonster) )
     assert( 9 <= fish.attack <= 11)
     assert( 9 <= fish.defense <= 11)
+
+# Testing to make sure the monster is now spawning too close to the player or inside of a wall
+def test_find_location_for_monster():
+    spawner = MonsterSpawner()
+    player = Player()
+    dungeon = DungeonMap()
+    dungeon.update_dungeon([player], [player])
+
+    fish = spawner.get_monster("fish", dungeon)
+
+    fish = spawner.find_location_for_monster(fish, dungeon, player)
+
+    # making sure they aren't in a wall or on a different actor
+    assert(dungeon.grid[fish.row][fish.col] == FLOOR)
+
+    # using the monster AI to tell how far we are from the player
+    assert( len(fish.ai.solve()) >= 10 )
