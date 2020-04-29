fb60c479d8d1fadd4e4dc86410329d6518ba1d32
Kayden <kayden.adams@colorado.edu>
2020-04-29 16:03:00 -0600

    Adds proof-of-concept for an instruction screen

--- arcade_main.py
+++ arcade_main.py
@@ -24,6 +24,24 @@ Detects if the direction the player is trying to move to is valid
 def player_collision(player, direction, map):
     return(map.get_tile_type(player.row+direction[0], player.col+direction[1]) != FLOOR)
 
+'''
+Code taken https://arcade.academy/examples/view_instructions_and_game_over.html#view-instructions-and-game-over
+'''
+class InstructionView(arcade.View):
+    def on_show(self):
+        arcade.set_background_color(arcade.color.ORANGE_PEEL)
+
+    def on_draw(self):
+        arcade.start_render()
+        arcade.draw_text("Instructions Screen", WIDTH/2, HEIGHT/2,
+                         arcade.color.BLACK, font_size=50, anchor_x="center")
+        arcade.draw_text("Click to advance", WIDTH/2, HEIGHT/2-75,
+                         arcade.color.GRAY, font_size=20, anchor_x="center")
+
+    def on_mouse_press(self, _x, _y, _button, _modifiers):
+        game_view = MyGame()
+        game_view.setup()
+        self.window.show_view(game_view)
 
 
 class MyGame(arcade.View):
@@ -298,9 +316,11 @@ class MyGame(arcade.View):
 def main():
     """ Main method """
     window = arcade.Window(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
-    game_view = MyGame()
-    game_view.setup()
-    window.show_view(game_view)
+    # game_view = MyGame()
+    # game_view.setup()
+    # window.show_view(game_view)
+    instrution_view = InstructionView()
+    window.show_view(instrution_view)
     arcade.run()
 
 
