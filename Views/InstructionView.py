b7e88a8bd8f5a175ea9ffd3126abf9a1a28e3f2a
Kayden <kayden.adams@colorado.edu>
2020-05-02 09:42:45 -0600

    Adds some text to the instructions screen

--- Views/InstructionView.py
+++ Views/InstructionView.py
@@ -1,5 +1,7 @@
 from definitions import arcade, SCREEN_WIDTH, SCREEN_HEIGHT
 from Views.GameView import GameView
+from Actors.Player import Player
+from Actors.Monster import *
 
 '''
 Code taken from https://arcade.academy/examples/view_instructions_and_game_over.html#view-instructions-and-game-over
@@ -7,14 +9,24 @@ A start screen that shows instructions on how to play the game.
 '''
 class InstructionView(arcade.View):
     def on_show(self):
-        arcade.set_background_color(arcade.color.ORANGE_PEEL)
+        arcade.set_background_color(arcade.color.AUROMETALSAURUS)
 
     def on_draw(self):
         arcade.start_render()
-        arcade.draw_text("Instructions Screen", SCREEN_WIDTH/2, SCREEN_HEIGHT/2,
+        arcade.draw_text("Instructions Screen", SCREEN_WIDTH/2, SCREEN_HEIGHT - 2*SCREEN_HEIGHT/10,
                          arcade.color.BLACK, font_size=50, anchor_x="center")
-        arcade.draw_text("Click to advance", SCREEN_WIDTH/2, SCREEN_HEIGHT/2-75,
-                         arcade.color.GRAY, font_size=20, anchor_x="center")
+        arcade.draw_text("You look like this: ", SCREEN_WIDTH/20, SCREEN_HEIGHT- 3.5*SCREEN_HEIGHT/10,
+                         arcade.color.BLACK, font_size=16, anchor_x="left")
+        arcade.draw_text("The monsters look like this: ", SCREEN_WIDTH/20, SCREEN_HEIGHT- 4.5*SCREEN_HEIGHT/10,
+                         arcade.color.BLACK, font_size=16, anchor_x="left")
+
+        arcade.draw_text("Survive as many turns as you can!", SCREEN_WIDTH/2, SCREEN_HEIGHT- 8.2*SCREEN_HEIGHT/10,
+                         arcade.color.BLACK, font_size=20, anchor_x="center")
+
+
+
+        arcade.draw_text("Click to advance", SCREEN_WIDTH/2, SCREEN_HEIGHT- 9*SCREEN_HEIGHT/10,
+                         arcade.color.BLACK, font_size=30, anchor_x="center")
 
     def on_mouse_press(self, _x, _y, _button, _modifiers):
         game_view = GameView()
